0168d7cffea1e7bf0bc36eb0cef55adb0f0c6bba
unknown <srazbash@MU00014478.AD.MONASH.EDU>
2011-11-01 17:28:29 +1100

    forecast 3.10 as published on CRAN  On branch master

---
+++ R/forecast.R
@@ -0,0 +1,258 @@
+## Generic forecast functions
+## Part of forecast and demography packages
+
+forecast <- function(object,...) UseMethod("forecast")
+
+forecast.default <- function(object,...) forecast.ts(object,...)
+
+forecast.ts <- function(object, h=ifelse(frequency(object)>1, 2*frequency(object), 10), level=c(80,95), fan=FALSE, ...)
+{
+  n <- length(object)
+  if(n > 3)
+  {
+    if(frequency(object) < 13)
+      forecast(ets(object,...),h=h,level=level,fan=fan)
+    else
+      stlf(object,h=h,level=level,fan=fan,...)
+  }
+  else 
+    meanf(object,h=h,level=level,fan=fan,...)
+}
+
+as.data.frame.forecast <- function(x,...)
+{
+    nconf <- length(x$level)
+    out <- matrix(x$mean, ncol=1)
+    ists <- is.ts(x$mean)
+    if(ists)
+    {
+        out <- ts(out)
+        attributes(out)$tsp <- attributes(x$mean)$tsp
+    }
+    names <- c("Point Forecast")
+    if (!is.null(x$lower) & !is.null(x$upper) & !is.null(x$level)) 
+    {
+        x$upper <- as.matrix(x$upper)
+        x$lower <- as.matrix(x$lower)
+        for (i in 1:nconf) 
+        {
+            out <- cbind(out, x$lower[, i], x$upper[, i])
+            names <- c(names, paste("Lo", x$level[i]), paste("Hi", x$level[i]))
+        }
+    }
+    colnames(out) <- names
+    rownames(out) <- time(x$mean)
+    # Rest of function borrowed from print.ts(), but with header() omitted
+    if(!ists)
+        return(as.data.frame(out))
+
+    x <- as.ts(out)
+    fr.x <- frequency(x)
+    calendar <- any(fr.x == c(4, 12)) && length(start(x)) ==  2L
+    Tsp <- tsp(x)
+    if (is.null(Tsp)) 
+    {
+        warning("series is corrupt, with no 'tsp' attribute")
+        print(unclass(x))
+        return(invisible(x))
+    }
+    nn <- 1 + round((Tsp[2L] - Tsp[1L]) * Tsp[3L])
+    if (NROW(x) != nn) 
+    {
+        warning(gettextf("series is corrupt: length %d with 'tsp' implying %d", NROW(x), nn), domain=NA, call.=FALSE)
+        calendar <- FALSE
+    }
+    if (NCOL(x) == 1) 
+    {
+        if (calendar) 
+        {
+            if (fr.x > 1) 
+            {
+                dn2 <- if (fr.x == 12) 
+                  month.abb
+                else if (fr.x == 4) 
+                  c("Qtr1", "Qtr2", "Qtr3", "Qtr4")
+                else paste("p", 1L:fr.x, sep="")
+                if (NROW(x) <= fr.x && start(x)[1L] == end(x)[1L]) 
+                {
+                  dn1 <- start(x)[1L]
+                  dn2 <- dn2[1 + (start(x)[2L] - 2 + seq_along(x))%%fr.x]
+                  x <- matrix(format(x, ...), nrow=1L, byrow=TRUE, 
+                    dimnames=list(dn1, dn2))
+                }
+                else 
+                {
+                  start.pad <- start(x)[2L] - 1
+                  end.pad <- fr.x - end(x)[2L]
+                  dn1 <- start(x)[1L]:end(x)[1L]
+                  x <- matrix(c(rep.int("", start.pad), format(x, ...), rep.int("", end.pad)), ncol=fr.x, 
+                    byrow=TRUE, dimnames=list(dn1, dn2))
+                }
+            }
+            else 
+            {
+                tx <- time(x)
+                attributes(x) <- NULL
+                names(x) <- tx
+            }
+        }
+        else
+            attr(x, "class") <- attr(x, "tsp") <- attr(x, "na.action") <- NULL
+    }
+    else 
+    {
+        if (calendar && fr.x > 1) 
+        {
+            tm <- time(x)
+            t2 <- 1 + round(fr.x * ((tm + 0.001)%%1))
+            p1 <- format(floor(zapsmall(tm)))
+            rownames(x) <- if (fr.x == 12) 
+                paste(month.abb[t2], p1, sep=" ")
+            else paste(p1, if (fr.x == 4) 
+                c("Q1", "Q2", "Q3", "Q4")[t2]
+            else format(t2), sep=" ")
+        }
+        else
+            rownames(x) <- format(time(x))
+        attr(x, "class") <- attr(x, "tsp") <- attr(x, "na.action") <- NULL
+    }
+    return(as.data.frame(x))
+}
+
+print.forecast <- function(x ,...)
+{
+    print(as.data.frame(x))
+}
+
+
+summary.forecast <- function(object,...)
+{
+    cat(paste("\nForecast method:",object$method))
+#    cat(paste("\n\nCall:\n",deparse(object$call)))
+    cat(paste("\n\nModel Information:\n"))
+    print(object$model)
+    cat("\nIn-sample error measures:\n")
+    print(accuracy(object))
+    if(is.null(object$mean))
+        cat("\n No forecasts\n")
+    else
+    {
+        cat("\nForecasts:\n")
+        print(object)
+    }
+}
+
+plot.forecast <- function(x, include, plot.conf=TRUE, shaded=TRUE, shadebars=(length(x$mean)<5),
+        shadecols=NULL, col=1, fcol=4, pi.col=1, pi.lty=2, ylim=NULL, main=NULL, ylab="",xlab="",...)
+{
+    if(is.element("x",names(x))) # Assume stored as x
+        data=as.ts(x$x)
+    else
+    {
+        data=NULL
+        include=0
+    }
+
+    if(missing(include))
+        include <- length(data)
+
+    if(is.null(x$lower) | is.null(x$upper) | is.null(x$level))
+        plot.conf=FALSE
+        
+    if(!shaded)
+        shadebars <- FALSE
+
+    # Extract components of predict if it exists
+    # This occurs for the pegels functions.
+    if(is.null(main))
+        main=paste("Forecasts from ",x$method,sep="")
+
+    freq <- frequency(data)
+    strt <- start(data)
+    n <- length(data)
+    if(plot.conf)
+    {
+        upper <- as.matrix(x$upper)
+        lower <- as.matrix(x$lower)
+    }
+    pred.mean <- x$mean
+    # if(!is.null(lambda))  # undo Box-Cox transformation
+    # {
+        # pred.mean <- InvBoxCox(x$mean,lambda)
+        # if(plot.conf)
+        # {
+            # lower <- InvBoxCox(lower,lambda)
+            # upper <- InvBoxCox(upper,lambda)
+        # }
+        # xx <- InvBoxCox(data,lambda)
+        # if(lambda<0 & plot.conf)
+        # {
+            # junk <- upper
+            # upper <- lower
+            # lower <- junk
+        # }
+    # }
+    # else
+        xx <- data
+    # Remove final missing values
+    nx <- max(which(!is.na(xx)))
+    xxx <- xx[1:nx]
+    include <- min(include,nx)
+    if(is.null(ylim))
+    {
+        ylim <- range(c(xx[(n-include+1):n],pred.mean),na.rm=TRUE)
+        if(plot.conf)
+            ylim <- range(ylim,lower,upper,na.rm=TRUE)
+    }
+    npred <- length(pred.mean)
+    plot(ts(c(xxx[(nx-include+1):nx], rep(NA, npred)), end=tsp(xx)[2] + (nx-n)/freq + npred/freq, f=freq),
+        xlab=xlab,ylim=ylim,ylab=ylab,main=main,col=col,...)
+
+    if(plot.conf)
+    {
+        xxx <- tsp(pred.mean)[1] - 1/freq + (1:npred)/freq            
+        idx <- rev(order(x$level))
+        nint <- length(x$level)
+        for(i in 1:nint)
+        {
+      if(is.null(shadecols))
+        shadecols <- heat.colors(length(x$level)+2)[switch(1+(length(x$level)>1),2,length(x$level):1)+1]
+            if(shadebars)
+      {
+                for(j in 1:npred)
+                {
+                    polygon(xxx[j] + c(-0.5,0.5,0.5,-0.5)/freq, c(rep(lower[j,idx[i]],2),rep(upper[j,idx[i]],2)),
+                        col=shadecols[i], border=FALSE)
+                }
+      }
+            else if(shaded)
+            {
+                polygon(c(xxx,rev(xxx)), c(lower[,idx[i]],rev(upper[,idx[i]])),
+                        col=shadecols[i], border=FALSE)
+            }
+            else if(npred == 1)
+            {
+                lines(xxx+c(-0.5,0.5)/freq,rep(lower[,idx[i]],2),col=pi.col,lty=pi.lty)
+                lines(xxx+c(-0.5,0.5)/freq,rep(upper[,idx[i]],2),col=pi.col,lty=pi.lty)
+            }
+            else
+            {
+                lines(xxx,lower[,idx[i]],col=pi.col,lty=pi.lty)
+                lines(xxx,upper[,idx[i]],col=pi.col,lty=pi.lty)
+            }
+        }
+    }
+    if(npred > 1 & !shadebars)
+        lines(pred.mean, lty=1,col=fcol)
+    else
+        points(pred.mean, col=fcol, pch=19)
+    if(plot.conf)
+        invisible(list(mean=pred.mean,lower=lower,upper=upper))
+    else
+        invisible(list(mean=pred.mean))
+}
+
+predict.default <- function(object, ...)
+{
+    forecast(object, ...)
+}
---
+++ R/season.R
@@ -0,0 +1,192 @@
+### Functions to handle seasonality
+
+monthdays <- function(x)
+{
+    if(!is.ts(x))
+        stop("Not a time series")
+    f <- frequency(x)
+    if(f==12)
+        days <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+    else if(f==4)
+        days <- c(90,91,92,92)
+    else
+        stop("Not monthly or quarterly data")
+    nyears <- round(length(x)/f+1)+1
+    years <- (1:nyears) + (start(x)[1] - 1)
+    leap.years <- ((years %% 4 == 0) & !(years %% 100 ==0 & years %% 400 != 0))[1:nyears]
+    dummy <- t(matrix(rep(days,nyears),nrow=f))
+    if(f==12)
+        dummy[leap.years,2] <- 29
+    else
+        dummy[leap.years,1] <- 91
+    xx <- c(t(dummy))[start(x)[2]-1+(1:length(x))]
+    return(ts(xx,start=start(x),f=f))
+}
+
+sindexf <- function(object,h)
+{
+    if(class(object)=="stl")
+    {
+        ss <- object$time.series[,1]
+        m <- frequency(ss)
+        ss <- ss[length(ss)-(m:1)+1]
+        tsp.x <- tsp(object$time.series)
+    }
+    else if(class(object)=="decomposed.ts")
+    {
+        ss <- object$figure
+        m <- frequency(object$seasonal)
+        n <- length(object$trend)
+        ss <- rep(ss,n/m+1)[1:n]
+        ss <- ss[n-(m:1)+1]
+        tsp.x <- tsp(object$seasonal)
+    }
+    else
+        stop("Object of unknown class")
+    out <- ts(rep(ss,h/m+1)[1:h], f=m, start=tsp.x[2]+1/m)
+
+    return(out)
+}
+
+seasadj <- function(object)
+{
+    if(class(object)=="stl")
+        return(object$time.series[,2]+object$time.series[,3])
+    else if(class(object)=="decomposed.ts")
+    {
+        if(object$type=="additive")
+            return(object$x-object$seasonal)
+        else
+            return(object$x/object$seasonal)
+    }
+    else
+        stop("Object of unknown class")
+}
+
+seasonaldummy <- function(x)
+{
+    if(!is.ts(x))
+        stop("Not a time series")
+    else
+        fr.x <- frequency(x)
+    if(fr.x==1)
+        stop("Non-seasonal time series")
+    dummy <- as.factor(cycle(x))
+    dummy.mat <- matrix(0,ncol=frequency(x)-1,nrow=length(x))
+    nrow <- 1:length(x)
+    for(i in 1:(frequency(x)-1))
+        dummy.mat[dummy==paste(i),i] =1
+    colnames(dummy.mat) <- if (fr.x == 12)
+                month.abb[1:11]
+            else if(fr.x == 4)
+                c("Q1", "Q2", "Q3")
+            else paste("S",1:(fr.x-1),sep="")
+
+    return(dummy.mat)
+}
+
+seasonaldummyf <- function(x, h)
+{
+    if(!is.ts(x))
+        stop("Not a time series")
+    f=frequency(x)
+    return(seasonaldummy(ts(rep(0,h),start=tsp(x)[2]+1/f,freq=f)))
+}
+
+forecast.stl <- function(object, method=c("ets","arima"),
+     h = frequency(object$time.series)*2, level = c(80, 95), fan = FALSE, lambda=NULL, ...)
+{
+  method <- match.arg(method)
+  m <- frequency(object$time.series)
+  n <- nrow(object$time.series)
+  lastseas <- rep(object$time.series[n-(m:1)+1,"seasonal"],max(1,h/(m-1)))[1:h]
+  # De-seasonalize
+  x.sa <- seasadj(object)
+  # Forecast
+  if(method=="ets")
+      fit <- ets(x.sa,model="ZZN",...)
+  else
+      fit <- auto.arima(x.sa,D=0,max.P=0,max.Q=0,...)
+  fcast <- forecast(fit,h=h,level=level,fan=fan)
+  # Reseasonalize
+  fcast$mean <- fcast$mean + lastseas
+  fcast$upper <- fcast$upper + lastseas
+  fcast$lower <- fcast$lower + lastseas
+  fcast$x <- ts(rowSums(object$time.series))
+  tsp(fcast$x) <- tsp(object$time.series)
+  fcast$method <- paste("STL + ",fcast$method)
+  fcast$seasonal <- ts(lastseas[1:m],f=m,start=tsp(object$time.series)[2]-1+1/m)
+  fcast$fitted <- fitted(fcast)+object$time.series[,1]
+  fcast$residuals <- fcast$x - fcast$fitted
+  
+	if (!is.null(lambda)) 
+	{
+		fcast$x <- InvBoxCox(fcast$x,lambda)
+		fcast$fitted <- InvBoxCox(fcast$fitted, lambda)
+		fcast$mean <- InvBoxCox(fcast$mean, lambda)
+		fcast$lower <- InvBoxCox(fcast$lower, lambda)
+		fcast$upper <- InvBoxCox(fcast$upper, lambda)
+		fcast$lambda <- lambda
+	}
+  
+   return(fcast)
+}
+
+stlf <- function(x ,h=frequency(x)*2, s.window=7, method=c("ets","arima"), lambda=NULL, level = c(80, 95), fan = FALSE, ...)
+{
+	if (!is.null(lambda)) 
+	{
+		origx <- x
+		x <- BoxCox(x, lambda)
+	}
+
+	fit <- stl(x,s.window=s.window)
+	fcast <- forecast(fit,h=h,method=method,level=level,fan=fan,...)
+
+	if (!is.null(lambda)) 
+	{
+		fcast$x <- origx
+		fcast$fitted <- InvBoxCox(fcast$fitted, lambda)
+		fcast$mean <- InvBoxCox(fcast$mean, lambda)
+		fcast$lower <- InvBoxCox(fcast$lower, lambda)
+		fcast$upper <- InvBoxCox(fcast$upper, lambda)
+		fcast$lambda <- lambda
+	}
+
+	return(fcast)
+}
+
+fourier <- function(x, K)
+{
+    n <- length(x)
+    period <- frequency(x)
+    X <- matrix(,nrow=n,ncol=2*K)
+    for(i in 1:K)
+    {
+        X[,2*i-1] <- sin(2*pi*i*(1:n)/period)
+        X[,2*i] <- cos(2*pi*i*(1:n)/period)
+    }
+    colnames(X) <- paste(c("S","C"),rep(1:K,rep(2,K)),sep="")
+    return(X)
+}
+
+fourierf <- function(x, K, h)
+{
+    n <- length(x)
+    period <- frequency(x)
+    X <- matrix(,nrow=h,ncol=2*K)
+    for(i in 1:K)
+    {
+        X[,2*i-1] <- sin(2*pi*i*((n+1):(n+h))/period)
+        X[,2*i] <- cos(2*pi*i*((n+1):(n+h))/period)
+    }
+    colnames(X) <- paste(c("S","C"),rep(1:K,rep(2,K)),sep="")
+    return(X)
+}
+
+#Changed definition of observed to use passed data rather than construct on fly
+plot.decomposed.ts <- function (x, ...) 
+{
+    plot(cbind(observed = x$x, trend = x$trend, seasonal = x$seasonal, random = x$random), 
+        main = paste("Decomposition of", x$type, "time series"), ...)
+}
