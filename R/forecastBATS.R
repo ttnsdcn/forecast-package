e3c54864cbc354d0ba9bc94ebd3a5971ab869d46
Rob Hyndman <robjhyndman@gmail.com>
2011-12-09 14:47:02 +1100

    Bug fix in forecast.bats() due to change in bats object structure. Note: start.time no longer returned as it is available in the tsp for y.

--- R/forecastBATS.R
+++ R/forecastBATS.R
@@ -74,8 +74,8 @@ forecast.bats <- function(object, h=10, level=c(80,95), fan=FALSE, ...)
 	y <- ts(c(object$y,0), start=start.time, frequency=ts.frequency)
 	fcast.start.time <- end(y)
 	#Make msts object for x and mean
-	x <- msts(object$y, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { 1}), ts.frequency=ts.frequency, start=object$start.time)
-	fitted.values <- msts(object$fitted.values, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { 1}), start=object$start.time)
+	x <- msts(object$y, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { 1}), ts.frequency=ts.frequency, start=start.time)
+	fitted.values <- msts(object$fitted.values, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { 1}), start=start.time)
 	y.forecast <- msts(y.forecast, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { 1}), start=fcast.start.time)
 		
 	forecast.object <- list(model=object, mean=y.forecast, level=level, x=x, upper=upper.bounds, lower=lower.bounds, fitted=fitted.values, method=makeText(object), residuals=object$errors)
